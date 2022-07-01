03fb53ff111a33344c101d691b14975a7e49e26b
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-07-01 13:44:26 -0500

    added navbar component with airbnb logo

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+//import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <nav>
+            {/* <img src="../images/airbnb-logo.png" className="nav--logo"/> */}
+            <img src="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-Logo.png" className="nav--logo" alt="airbnb logo"/>
+        </nav>
+    )
+};
+
+export default Navbar;
