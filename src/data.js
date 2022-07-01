d9a6a6425b9e4010aeb7327bb5d1912164218521
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-07-01 16:31:11 -0500

    added card component to app

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export default [
+    {
+        id: 1,
+        title: "Yoga Lessons with Jane Smith",
+        description: "Yoga is essential in maintaining a balanced and healthy lifestyle",
+        price: 120,
+        coverImg: "https://static01.nyt.com/images/2016/12/02/well/move/yoga_body_images-slide-HNVD/yoga_body_images-slide-HNVD-square320-v3.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 10
+        },
+        location: "Online",
+        openSpots: 1,
+    },
+    {
+        id: 2,
+        title: "Learn Nature Photography",
+        description: "Interested in becoming a nature photographer? Join us in learning techniques needed to capture nature with memories that'll last a lifetime.",
+        price: 150,
+        coverImg: "https://www.lukas-petereit.com/wp-content/uploads/2017/10/Rakotzbr%C3%BCcke-Bridge-Rakotz-Kromlau-Lake-Sun-Sunrise-Landscape-Reflection-Germany-Saxony-Travel-Photography-Nature-Photo-Spreewald-2.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 20
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Hiking",
+        description: "Experience the beautiful Swiss landscape and meet new friends all while conquering rugged terrain on your memorable trek!",
+        price: 60,
+        coverImg: "https://ak-d.tripcdn.com/images/100o1a0000018xtj56B0C.jpg",
+        stats: {
+            rating: 4.7,
+            reviewCount: 15
+        },
+        location: "Switzerland",
+        openSpots: 2,
+    }
+]
